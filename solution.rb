2d7a0d55ba419745c8e764a22ac4f94e8f522cd6
Juan Pablo <dupjpr@gmail.com>
2020-05-04 22:18:13 -0500

    4 actualitation

--- solution.rb
+++ solution.rb
@@ -1,23 +1,45 @@
 require"sinatra"
 
+
 get  '/' do
 
   
     unless params[:name]
         "Hola desconocido!"
     else
-        if params[:name] == ""
+        if params[:name].length == 0
            "Hola desconocido!"
         else
-           
             params[:name]
-          
         end
     end
-    # @tag1 = tag1
-    @name = params[:name]
-   
-    erb :index
+   @name = params[:name]
 
+   erb :index
+   
 end
+
+
+
+# require"sinatra"
+
+
+# get  '/' do
+
+  
+#     unless params[:name]
+#         "Hola desconocido!"
+#     else
+#         if params[:name].length == 0
+#            "Hola desconocido!"
+#         else
+           
+#             "Hola #{params[:name]}!"
+          
+#         end
+#     end
+   
+   
+# end
+
 
